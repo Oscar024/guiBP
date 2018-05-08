dbf128d107c47df09368bc7dd91c2f16c7b9eaf4
Oscar024 <or.carvajal.ortiz@gmail.com>
2018-05-08 12:17:50 -0700

    serial interface agregacion del pulso

--- guibp.py
+++ guibp.py
@@ -8,7 +8,6 @@
 
 from PyQt4 import QtCore, QtGui
 
-
 import serial
 ser = serial.Serial('/dev/ttyUSB0', 9600)
 
@@ -33,27 +32,32 @@ class Ui_MainWindow(object):
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
         self.btnOpen = QtGui.QPushButton(self.centralwidget)
-        self.btnOpen.setGeometry(QtCore.QRect(460, 410, 99, 27))
+        self.btnOpen.setGeometry(QtCore.QRect(290, 400, 99, 27))
         self.btnOpen.setObjectName(_fromUtf8("btnOpen"))
         self.txtsys = QtGui.QLineEdit(self.centralwidget)
-        self.txtsys.setGeometry(QtCore.QRect(280, 260, 113, 27))
-        self.txtsys.setText(_fromUtf8(""))
+        self.txtsys.setGeometry(QtCore.QRect(300, 50, 113, 27))
         self.txtsys.setObjectName(_fromUtf8("txtsys"))
         self.btnClose = QtGui.QPushButton(self.centralwidget)
-        self.btnClose.setGeometry(QtCore.QRect(290, 410, 99, 27))
+        self.btnClose.setGeometry(QtCore.QRect(140, 400, 99, 27))
         self.btnClose.setObjectName(_fromUtf8("btnClose"))
         self.txtDias = QtGui.QLineEdit(self.centralwidget)
-        self.txtDias.setGeometry(QtCore.QRect(280, 190, 113, 27))
+        self.txtDias.setGeometry(QtCore.QRect(300, 80, 113, 27))
         self.txtDias.setObjectName(_fromUtf8("txtDias"))
         self.label = QtGui.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(170, 200, 68, 17))
+        self.label.setGeometry(QtCore.QRect(190, 90, 68, 17))
         self.label.setObjectName(_fromUtf8("label"))
         self.label_2 = QtGui.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(170, 260, 68, 17))
+        self.label_2.setGeometry(QtCore.QRect(190, 50, 68, 17))
         self.label_2.setObjectName(_fromUtf8("label_2"))
         self.btnRead = QtGui.QPushButton(self.centralwidget)
-        self.btnRead.setGeometry(QtCore.QRect(370, 340, 99, 27))
+        self.btnRead.setGeometry(QtCore.QRect(290, 180, 99, 27))
         self.btnRead.setObjectName(_fromUtf8("btnRead"))
+        self.txtPulso = QtGui.QLineEdit(self.centralwidget)
+        self.txtPulso.setGeometry(QtCore.QRect(300, 120, 113, 27))
+        self.txtPulso.setObjectName(_fromUtf8("txtPulso"))
+        self.lblpulso = QtGui.QLabel(self.centralwidget)
+        self.lblpulso.setGeometry(QtCore.QRect(190, 120, 68, 17))
+        self.lblpulso.setObjectName(_fromUtf8("lblpulso"))
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtGui.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 25))
@@ -70,16 +74,17 @@ class Ui_MainWindow(object):
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def read(self):
-        data = ser.readline()
-        if data:
-           print(data)
-        datos = list(data)
-        print(datos)
-        siastolica = datos[1]+datos[2]+datos[3]
-        diastolica = datos[6]+ datos[7]+datos[8]
-        pulso = datos[11]+datos[12]+datos[13]
-        self.txtsys.setText(_translate("MainWindow", siastolica, None))
-        self.txtDias.setText(_translate("MainWindow", diastolica, None))
+            data = ser.readline()
+            if data:
+                print(data)
+            datos = list(data)
+            print(datos)
+            siastolica = datos[1] + datos[2] + datos[3]
+            diastolica = datos[6] + datos[7] + datos[8]
+            pulso = datos[11] + datos[12] + datos[13]
+            self.txtsys.setText(_translate("MainWindow", siastolica, None))
+            self.txtDias.setText(_translate("MainWindow", diastolica, None))
+            self.txtPulso.setText(_translate("MainWindow", pulso, None))
 
     def close(self):
             ser.close()
@@ -87,17 +92,20 @@ class Ui_MainWindow(object):
 
     def open(self):
             ser.open()
-            print("opened")
 
-
+    print("opened")
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
         self.btnOpen.setText(_translate("MainWindow", "Open Port", None))
+        self.txtsys.setText(_translate("MainWindow", "0", None))
         self.btnClose.setText(_translate("MainWindow", "Close Port", None))
+        self.txtDias.setText(_translate("MainWindow", "0", None))
         self.label.setText(_translate("MainWindow", "Diastolic", None))
         self.label_2.setText(_translate("MainWindow", "Systolic", None))
         self.btnRead.setText(_translate("MainWindow", "Leer", None))
+        self.txtPulso.setText(_translate("MainWindow", "0", None))
+        self.lblpulso.setText(_translate("MainWindow", "Pulse", None))
 
 
 if __name__ == "__main__":
